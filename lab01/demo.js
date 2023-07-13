56d6b017ddcdd2bd42921bba74726f18a62da81a
trumk <nguyentrungtest2292003@gmail.com>
2023-07-13 20:23:06 +0700

    change location

---
+++ lab01/demo.js
@@ -0,0 +1,48 @@
+//single line comment
+/*mutiple 
+lines
+comment
+*/
+//trong javascript nói chung và nodejs nói riêng thì không 
+//bắt buộc phải viết dấu chấm phẩy ở cuối dòng
+//và để in ra text thì dấu nháy đơn hay nháy kép đều được
+console.log("Hello world");
+console.log('We are learning cloud computing')
+console.log('Today is Sunday')
+
+a = 17   //integer
+b = 3
+x = 3.14 //double
+c = a + b
+d = a - b
+e = a / b
+f = a * b
+g = a % b   //get remainder of a division
+
+console.log("c = "+c)
+console.log("d = "+d)
+console.log("e = "+e)
+console.log("f = "+f)
+console.log("g = "+g)
+
+full_name = "Nguyen Xuan Trung"     //string
+email_address = "trungnxgch211026@fpt.edu.vn"
+job = "Student"
+
+console.log('Name: '+full_name)
+console.log('Email: '+email_address)
+console.log('Job: '+job)
+
+check = true //boolean
+gender = 'm' //character
+
+a = 10
+b = a++     //postfix operator: b=a=10 => a++ => a=11
+c = ++a     //prefix operator: ++a = a+ 1= 12 => c=a=12
+console.log("a = "+a)
+console.log("b = "+b)
+console.log("c = "+c)
+
+x = 5
+x = x + 3   //x +=3
+x = x * 5   //x *=5
