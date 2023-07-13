dcaa0fbd75bac12d7028148dc2362c9d2e98dd69
trumk <nguyentrungtest2292003@gmail.com>
2023-07-13 11:58:40 +0700

    add video

--- web.js
+++ web.js
@@ -5,11 +5,13 @@ const host = 'localhost'
 //3. Khai báo port của server
 const port = 5000       //3000: default port of NodeJS server
 //4. Khai báo server
-const server = http.createServer((request, respond) =>{
-    respond.write("<h1 style='color: red; background-color: pink' >Hello world</h1>")
-    respond.write("<h3 style='font-style: italic; text-decoration: underline'>We are from Greenwich Vietnam</h3>")
-    respond.end
-})
+const server = http.createServer((request, response) => {
+    response.write("<h1 style='color: red; background-color: pink'>Hello world</h1>");
+    response.write("<h3 style='font-style: italic; text-decoration: underline'>We are from Greenwich Vietnam</h3>");
+    response.write("<h2 style='font-style: italic; text-decoration: underline'>Thanh ba ga</h2>");
+    response.write("<iframe width=\"1300\" height=\"700\" src=\"https://www.youtube.com/embed/uZQJPclKn2E\"></iframe>");
+    response.end();
+  });
 //5. Cho server listen port để chạy web
 server.listen(port, () => {
     console.log("Server is running at http://"+host+":"+port)
