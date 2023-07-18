5a6ff3559c2db131fb1359f61344aa25e5b48554
trumk <nguyentrungtest2292003@gmail.com>
2023-07-18 10:54:29 +0700

    switch case for each page

---
+++ lab02/node/web3.js
@@ -0,0 +1,40 @@
+const http = require('http')
+const port = 3000
+const fs = require('fs')
+const server = http.createServer((req, res)=>{
+    res.statusCode = 200;
+    res.setHeader("Contain-Type", "text/html")
+    switch(req.url){
+        case "/":
+            fs.readFile("index.html", (err, data) =>{
+                if (err){
+                    console.log(err)
+                }else{
+                    res.writeHead(200)
+                    res.write(data)
+                    res.end()
+                }    
+                
+            })
+            break;
+        case "/login":
+            fs.readFile("login.html", (err, data) =>{
+                if (err){
+                    console.log(err)
+                }else{
+                    res.writeHead(200)
+                    res.write(data)
+                    res.end()
+                }    
+                
+            })
+            break;
+        default:
+            res.write("<h1>404 - Not found</h1>")
+            res.end()
+            break;
+    }
+})
+server.listen(port, ()=>{
+    console.log("Server is listening at http://localhost:"+port)
+})
