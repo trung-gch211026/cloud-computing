5af7a3e31517b8c8a84ce9b6dd99b23ab3eed6b3
trumk <nguyentrungtest2292003@gmail.com>
2023-07-18 11:01:46 +0700

    link to other page

--- lab02/node/web3.js
+++ lab02/node/web3.js
@@ -29,6 +29,18 @@ const server = http.createServer((req, res)=>{
                 
             })
             break;
+        case "/about":
+            fs.readFile("about.html", (err, data) =>{
+                if (err){
+                    console.log(err)
+                }else{
+                    res.writeHead(200)
+                    res.write(data)
+                    res.end()
+                }    
+                
+            })
+            break;
         default:
             res.write("<h1>404 - Not found</h1>")
             res.end()
