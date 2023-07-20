5c8f56dfe92fb5c54e7705dc56775a895f133cb1
trumk <trungnxgch211026@fpt.edu.vn>
2023-07-20 10:38:26 +0700

    pass data from routes to view

--- lab03/web03/app.js
+++ lab03/web03/app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var demoRouter = require('./routes/demo')
+var demoRouter = require('./routes/demo');
+var greenwichRouter = require('./routes/greenwich');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/demo', demoRouter);
+app.use('/fgw', greenwichRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
