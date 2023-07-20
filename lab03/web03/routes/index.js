351c0367de01ff6967d519aa6baf1771f90d0390
trumk <trungnxgch211026@fpt.edu.vn>
2023-07-20 10:55:25 +0700

    pass mutiple variable from routes to view

--- lab03/web03/routes/index.js
+++ lab03/web03/routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', (req, res)=>{
+  var country = "Viet Nam"
+  var year = 2023
+  var grade = 7.5
+  var pass = true
+  var gender = 'm'
+  res.render('index',
+  {
+    country: country,
+    year:year,
+    grade:grade,
+    pass: pass,
+    gender: gender
+  })
+})
 
 module.exports = router;
