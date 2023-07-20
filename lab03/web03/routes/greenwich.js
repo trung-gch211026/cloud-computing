9208d6c99cca69a9587d7e298baa7292721194d1
trumk <trungnxgch211026@fpt.edu.vn>
2023-07-20 10:47:49 +0700

    pass variable img to view

--- lab03/web03/routes/greenwich.js
+++ lab03/web03/routes/greenwich.js
@@ -4,7 +4,8 @@ var router = express.Router()
 router.get('/hn', (req, res)=>{
     var hanoi = "Ha Noi"
     var address = "So 2 Pham Van Bach - Cau Giay - Ha Noi"
-    res.render('hanoi', {hn : hanoi, address : address})
+    var image = "https://vcdn-vnexpress.vnecdn.net/2022/06/03/Image-249300564-ExtractWord-0-8746-7982-1654231437.png"
+    res.render('hanoi', {hn : hanoi, address : address, img:image})
 })
 
 router.get('/hcm', (req, res)=>{
