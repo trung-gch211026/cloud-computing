fa3fbc8e323cb8f1971be2494aabee84330f85b6
trumk <trungnxgch211026@fpt.edu.vn>
2023-07-25 10:49:01 +0700

    create object array list and pass to view

---
+++ lab04/web01/models/Mobile.js
@@ -0,0 +1,8 @@
+class Mobile{
+    constructor(no, product, img){
+        this.no = no
+        this.product = product
+        this.img = img
+    }
+}
+module.exports = Mobile;
--- lab04/web01/routes/mobile.js
+++ lab04/web01/routes/mobile.js
@@ -1,8 +1,22 @@
-const express = require('express')
-const router = express.Router()
+var express = require('express')
+var router = express.Router()
+var Mobile = require('../models/Mobile');
+
 
 router.get('/', (req, res)=>{
-    res.render('mobile/index')
+    var text = "Mobile evolution"
+    var year = 2023
+
+    res.render('mobile/index', {text:text, nam:year})
 })
 
-module.exports = router;
+router.get('/list', (req, res)=>{
+    var mobile1 = new Mobile(1,"iphone 3", 'https://cdn.thegioididong.com/Products/Images/42/22230/iPhone-3GS-8GB-1.jpg')
+    var mobile2 = new Mobile(2,"iphone 6", 'https://static.chotot.com/storage/chotot-kinhnghiem/c2c/2020/07/iphone-6s-plus-ra-mat-nam-nao-1.jpg')
+    var mobile3 = new Mobile(3,"iphone 14 promax", 'https://cdn.viettelstore.vn/Images/Product/ProductImage/1896224739.jpeg')
+    var mobileList = [mobile1, mobile2, mobile3]
+
+    res.render('mobile/list', {product:mobileList})
+})
+
+module.exports = router; 
