3cbfc43b0499c792b4432e8daea6a520bae3e8f4
trumk <nguyentrungtest2292003@gmail.com>
2023-08-02 21:45:59 +0700

    create route for student list

--- lab06/web01/routes/index.js
+++ lab06/web01/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express')
 const BookModel = require('../models/BookModel')
+const StudentModel = require('../models/StudentModel')
 var router = express.Router()
 
 router.get('/book', async (req, res)=>{
@@ -8,6 +9,11 @@ router.get('/book', async (req, res)=>{
   //console.log(books)
   //res.send(books)
   res.render('book_list', {books : books})
+})
+
+router.get('/student', async (req, res)=>{
+  var students = await StudentModel.find()
+  res.render('student_list', {students : students})
 })
 
 module.exports = router
