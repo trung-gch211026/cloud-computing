b39a93fa92661f90e9e2296e7ae02734f35aa7c4
trumk <nguyentrungtest2292003@gmail.com>
2023-08-03 11:01:27 +0700

    delete

--- lab07/web01/routes/mobile.js
+++ lab07/web01/routes/mobile.js
@@ -1,10 +1,35 @@
-var express = require('express')
-const MobileModel = require('../models/MobileModel')
-var router = express.Router()
+var express = require("express");
+const MobileModel = require("../models/MobileModel");
+var router = express.Router();
 
-router.get('/', async (req, res)=>{
-    var mobiles = await MobileModel.find();
-    res.render('mobile/index', {mobiles:mobiles})
+router.get("/", async (req, res) => {
+  var mobiles = await MobileModel.find();
+  res.render("mobile/index", { mobiles: mobiles });
+});
+
+router.get("/delete/:id", async (req, res) => {
+  var id = req.params.id;
+
+  //cach 1
+  await MobileModel.findByIdAndDelete(id)
+    .then(() => console.log("Delete success"))
+    .catch((error) => console.log("Delete failed"));
+
+  //cach 2
+  // var mobile = await MobileModel.findById(id)
+  // await MobileModel.remove(mobile)
+  // .then(()=>console.log('Delete success'))
+  // .catch((error)=>console.log('Delete failed'))
+
+  //redirect ve mobile sau khi xoa
+  res.redirect("/mobile");
+});
+
+router.get('/deleteall', async (req, res)=>{
+    await MobileModel.deleteMany()
+    .then(() => console.log("Delete success"))
+    .catch((error) => console.log("Delete failed"));
+    res.redirect("/mobile");
 })
 
-module.exports = router
+module.exports = router;
