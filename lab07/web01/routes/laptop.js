55e149f3c41d1033d72bde9d36f870e05990c128
trumk <nguyentrungtest2292003@gmail.com>
2023-08-07 21:03:32 +0700

    route index

--- lab07/web01/routes/laptop.js
+++ lab07/web01/routes/laptop.js
@@ -1,6 +1,9 @@
 var express = require('express')
 var router = express.Router()
 
-
+router.get("/", async (req, res) => {
+    var mobiles = await MobileModel.find();
+    res.render("mobile/index", { mobiles: mobiles });
+  });
 
 module.exports = router
