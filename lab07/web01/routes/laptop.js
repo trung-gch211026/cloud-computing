c8d271ca67b1c5f77ca9ce8321497f396bb67a10
trumk <nguyentrungtest2292003@gmail.com>
2023-08-07 21:22:05 +0700

    detail page

--- lab07/web01/routes/laptop.js
+++ lab07/web01/routes/laptop.js
@@ -1,11 +1,16 @@
-var express = require('express')
+var express = require("express");
 const LaptopModel = require("../models/LaptopModel");
-var router = express.Router()
-
+var router = express.Router();
 
 router.get("/", async (req, res) => {
-    var laptop = await LaptopModel.find();
-    res.render("laptop/index", { laptop: laptop });
-  });
+  var laptop = await LaptopModel.find();
+  res.render("laptop/index", { laptop: laptop });
+});
 
-module.exports = router 
+router.get("/detail/:id", async (req, res) => {
+  var id = req.params.id;
+  var laptop = await LaptopModel.findById(id);
+  res.render("laptop/detail", { laptop: laptop });
+});
+
+module.exports = router;
