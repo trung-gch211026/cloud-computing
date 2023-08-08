20f996088caa9d6428c6a6ac05e7e406379bb6d0
trumk <nguyentrungtest2292003@gmail.com>
2023-08-08 10:56:02 +0700

    route add

--- lab08/web01/routes/student.js
+++ lab08/web01/routes/student.js
@@ -10,12 +10,14 @@ router.get('/', async (req, res)=>{
 router.get('/delete/:id', async(req,res)=>{
     var id = req.params.id
     await StudentModel.findByIdAndDelete(id)
+    .then(() => console.log("Delete successfully"))
+    .catch((error) => console.log("Delete failed"));
     res.redirect('/student')
 })
 
 router.get('/deleteall', async (req, res)=>{
     await StudentModel.deleteMany()
-    .then(() => console.log("Delete success"))
+    .then(() => console.log("Delete successfully"))
     .catch((error) => console.log("Delete failed"));
     res.redirect("/student");
 })
@@ -25,7 +27,11 @@ router.get('/add', (req, res)=>{
 })
 
 router.post('/add', async (req, res)=>{
-    
+    var student = req.body
+    await StudentModel.create(student)
+    .then(console.log('Add successfully'))
+    .catch(err=>console.log(err))
+    res.redirect("/student")
 })
 
 module.exports = router;
