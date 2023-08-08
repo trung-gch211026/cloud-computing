c3416e786051b0a3bd358df0558e7bfcb6cdfbd4
trumk <nguyentrungtest2292003@gmail.com>
2023-08-08 10:33:58 +0700

    form add

--- lab08/web01/routes/student.js
+++ lab08/web01/routes/student.js
@@ -7,4 +7,25 @@ router.get('/', async (req, res)=>{
     res.render('student/studentList', {students: students})
 })
 
+router.get('/delete/:id', async(req,res)=>{
+    var id = req.params.id
+    await StudentModel.findByIdAndDelete(id)
+    res.redirect('/student')
+})
+
+router.get('/deleteall', async (req, res)=>{
+    await StudentModel.deleteMany()
+    .then(() => console.log("Delete success"))
+    .catch((error) => console.log("Delete failed"));
+    res.redirect("/student");
+})
+
+router.get('/add', (req, res)=>{
+    res.render('student/studentAdd')
+})
+
+router.post('/add', async (req, res)=>{
+    
+})
+
 module.exports = router;
