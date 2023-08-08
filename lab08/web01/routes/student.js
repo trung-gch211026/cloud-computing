4d22902f03a74c72f82858b42c746c432f8d7d4d
trumk <nguyentrungtest2292003@gmail.com>
2023-08-08 11:51:45 +0700

    edit

--- lab08/web01/routes/student.js
+++ lab08/web01/routes/student.js
@@ -34,4 +34,17 @@ router.post('/add', async (req, res)=>{
     res.redirect("/student")
 })
 
+router.get('/edit/:id', async (req, res) => {
+    var id = req.params.id;
+    var student = await StudentModel.findById(id);
+    res.render('student/studentEdit', { student: student });
+});
+
+router.post('/edit/:id', async (req, res)=>{
+    var id = req.params.id;
+    var student = req.body;
+    await StudentModel.findByIdAndUpdate(id, student);
+    res.redirect('/student');
+})
+
 module.exports = router;
