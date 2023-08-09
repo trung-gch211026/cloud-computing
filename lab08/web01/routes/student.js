40daeebfa979a40331e9bae750dcc1f931cdc074
trumk <nguyentrungtest2292003@gmail.com>
2023-08-09 20:13:04 +0700

    use multer

--- lab08/web01/routes/student.js
+++ lab08/web01/routes/student.js
@@ -1,69 +1,80 @@
-var express = require('express');
-const StudentModel = require('../models/StudentModel');
+var express = require("express");
+const StudentModel = require("../models/StudentModel");
+const multer = require("multer");
 var router = express.Router();
 
-router.get('/', async (req, res)=>{
-    var students = await StudentModel.find();
-    res.render('student/studentList', {students: students})
-})
-
-router.get('/delete/:id', async(req,res)=>{
-    var id = req.params.id
-    await StudentModel.findByIdAndDelete(id)
-    .then(() => console.log("Delete successfully"))
-    .catch((error) => console.log("Delete failed"));
-    res.redirect('/student')
-})
-
-router.get('/deleteall', async (req, res)=>{
-    await StudentModel.deleteMany()
-    .then(() => console.log("Delete successfully"))
-    .catch((error) => console.log("Delete failed"));
-    res.redirect("/student");
-})
-
-router.get('/add', (req, res)=>{
-    res.render('student/studentAdd')
-})
-
-router.post('/add', async (req, res)=>{
-    var student = req.body
-    await StudentModel.create(student)
-    .then(console.log('Add successfully'))
-    .catch(err=>console.log(err))
-    res.redirect("/student")
-})
-
-router.get('/edit/:id', async (req, res) => {
-    var id = req.params.id;
-    var student = await StudentModel.findById(id);
-    var formattedStudent = {
-        ...student.toObject(),
-        formattedDob: student.dob ? student.dob.toISOString().substring(0, 10) : '',
-        selectedMale: student.gender === 'Male' ? 'checked' : '',
-        selectedFemale: student.gender === 'Female' ? 'checked' : '',
-        selectedIT: student.department === 'IT' ? 'selected' : '',
-        selectedBusiness: student.department === 'Business' ? 'selected' : '',
-        selectedGraphicDesign: student.department === 'Graphic Design' ? 'selected' : ''
-    };
-    res.render('student/studentEdit', { student: formattedStudent });
+router.get("/", async (req, res) => {
+  var students = await StudentModel.find();
+  res.render("student/studentList", { students: students });
 });
 
+router.get("/delete/:id", async (req, res) => {
+  var id = req.params.id;
+  await StudentModel.findByIdAndDelete(id)
+    .then(() => console.log("Delete successfully"))
+    .catch((error) => console.log("Delete failed"));
+  res.redirect("/student");
+});
 
-router.post('/edit/:id', async (req, res) => {
-    var id = req.params.id;
-    var updatedStudent = req.body;
+router.get("/deleteall", async (req, res) => {
+  await StudentModel.deleteMany()
+    .then(() => console.log("Delete successfully"))
+    .catch((error) => console.log("Delete failed"));
+  res.redirect("/student");
+});
 
-    var originalStudent = await StudentModel.findById(id);
+router.get("/add", (req, res) => {
+  res.render("student/studentAdd");
+});
 
-    Object.keys(updatedStudent).forEach(key => {
-        if (updatedStudent[key] !== '' && updatedStudent[key] !== undefined) {
-            originalStudent[key] = updatedStudent[key];
-        }
-    });
+var storage = multer.diskStorage({
+  destination: (req, file, res) => {
+    res(null, "./public/images");
+  },
+  filename: (req, file, res) => {
+    res(null, file.originalname);
+  },
+});
+var upload = multer({ storage: storage });
 
-    await originalStudent.save();
-    res.redirect('/student');
+router.post("/add", upload.single("image"), async (req, res) => {
+  var student = req.body;
+  await StudentModel.create(student)
+    .then(console.log("Add successfully"))
+    .catch((err) => console.log(err));
+  res.redirect("/student");
+});
+
+router.get("/edit/:id", async (req, res) => {
+  var id = req.params.id;
+  var student = await StudentModel.findById(id);
+  var formattedStudent = {
+    ...student.toObject(),
+    formattedDob: student.dob ? student.dob.toISOString().substring(0, 10) : "",
+    selectedMale: student.gender === "Male" ? "checked" : "",
+    selectedFemale: student.gender === "Female" ? "checked" : "",
+    selectedIT: student.department === "IT" ? "selected" : "",
+    selectedBusiness: student.department === "Business" ? "selected" : "",
+    selectedGraphicDesign:
+      student.department === "Graphic Design" ? "selected" : "",
+  };
+  res.render("student/studentEdit", { student: formattedStudent });
+});
+
+router.post("/edit/:id", async (req, res) => {
+  var id = req.params.id;
+  var updatedStudent = req.body;
+
+  var originalStudent = await StudentModel.findById(id);
+
+  Object.keys(updatedStudent).forEach((key) => {
+    if (updatedStudent[key] !== "" && updatedStudent[key] !== undefined) {
+      originalStudent[key] = updatedStudent[key];
+    }
+  });
+
+  await originalStudent.save();
+  res.redirect("/student");
 });
 
 module.exports = router;
